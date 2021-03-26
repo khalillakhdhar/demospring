29a4482951adfa942d6732f4ba5bd65f15d9be99
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-26 10:15:58 +0100

    Initial commit

---
+++ src/main/java/com/projet/demospring/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.projet.demospring.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projet.demospring.model.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
+	@RequestMapping("secondepage")
+	public String page2()
+	{
+		return "second";
+		
+		
+	}
+	@RequestMapping(value = "user",method = RequestMethod.GET)
+	public String getuser(Model m)
+	{
+		User u=new User("teste","teste", 33);
+		User value=u;
+		m.addAttribute("current",value);
+		
+		return "user";
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/user.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+l'utilisateur est:<br>
+${ current.toString() }
+<a href="./">Home page</a>
+
+</body>
+</html>
